47c7d04f303accfc7d4e78d6acd82d80a72073a8
Sriram Sendhil <sriramsendhil@gmail.com>
2024-07-18 00:29:59 -0500

    mobile issues

--- app/components/ordersummary.tsx
+++ app/components/ordersummary.tsx
@@ -99,10 +99,12 @@ const Summary = () => {
             </div>
           </div>
         </div>
-        <div className="opacity-0 md:opacity-100 fixed bottom-4 right-4 mt-6 pt-4 w-auto flex flex-col items-center justify-center">
-          <img className="w-12" src="./assets/cubeLogo.svg" alt="Flying Package" />
-          <p className="text-gray-500">powered by Thryft Ship</p>
+        <div className="md:flex md:opacity-100 fixed top-12 left-4 md:bottom-4 md:right-4 mt-6 pt-4 w-auto flex-col items-center justify-center z-50">
+            <img className="w-12" src="./assets/cubeLogo.svg" alt="Flying Package" />
+            <p className="text-gray-500">powered by Thryft Ship</p>
         </div>
+
+
       </div>
     </Suspense>
   );
